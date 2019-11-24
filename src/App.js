26d48770bbf13e8cefacb781486807ae3993af7f
RafaVrech <rafavrech@gmail.com>
2019-11-24 01:12:26 -0300

    Ajustando a home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './css/pure-min.css';
 import './css/marketing.css';
+import './App.css';
 
 import Header from './componentes/Header';
 
@@ -8,131 +9,131 @@ class App extends Component{
 
   render(){
    return (
-      <div>
-          <Header/>
+    <div>
+        <Header/>
 
-          <div className="splash-container">
-              <div className="splash">
-                  <h1 className="splash-head">Easy Blood</h1>
-                  <p className="splash-subhead">
+        <div className="splash-container">
+            <div className="splash">
+                <h1 className="splash-head">
+                <img src="../logo.png"></img>
+                <p className="splash-subhead">
                     Existe um centro coletor precisando de você!
-                  </p>
-                  <p>
-                      <a href="#" className="pure-button pure-button-primary buscar" >Buscar</a>
-                  </p>
-              </div>
-          </div>
+                </p>
+                </h1>
+                <p>
+                    <a href="#" className="pure-button pure-button-primary buscar" >Buscar</a>
+                </p>
+            </div>
+        </div>
 
-      <div className="content-wrapper">
-          <div className="content">
-              <h2 className="content-head is-center">Excepteur sint occaecat cupidatat.</h2>
+        <div className="content-wrapper">
+            <div className="content">
+                <h2 className="content-head is-center">Excepteur sint occaecat cupidatat.</h2>
 
-              <div className="pure-g">
-                  <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <div className="pure-g">
+                    <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
 
-                      <h3 className="content-subhead">
-                          <i className="fa fa-rocket"></i>
-                          Get Started Quickly
-                      </h3>
-                      <p>
-                          Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
-                      </p>
-                  </div>
-                  <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
-                      <h3 className="content-subhead">
-                          <i className="fa fa-mobile"></i>
-                          Responsive Layouts
-                      </h3>
-                      <p>
-                          Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
-                      </p>
-                  </div>
-                  <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
-                      <h3 className="content-subhead">
-                          <i className="fa fa-th-large"></i>
-                          Modular
-                      </h3>
-                      <p>
-                          Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
-                      </p>
-                  </div>
-                  <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
-                      <h3 className="content-subhead">
-                          <i className="fa fa-check-square-o"></i>
-                          Plays Nice
-                      </h3>
-                      <p>
-                          Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
-                      </p>
-                  </div>
-              </div>
-          </div>
+                        <h3 className="content-subhead">
+                            <i className="fa fa-rocket"></i>
+                            Get Started Quickly
+                        </h3>
+                        <p>
+                            Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                        </p>
+                    </div>
+                    <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                        <h3 className="content-subhead">
+                            <i className="fa fa-mobile"></i>
+                            Responsive Layouts
+                        </h3>
+                        <p>
+                            Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                        </p>
+                    </div>
+                    <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                        <h3 className="content-subhead">
+                            <i className="fa fa-th-large"></i>
+                            Modular
+                        </h3>
+                        <p>
+                            Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                        </p>
+                    </div>
+                    <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                        <h3 className="content-subhead">
+                            <i className="fa fa-check-square-o"></i>
+                            Plays Nice
+                        </h3>
+                        <p>
+                            Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
-          <div className="ribbon l-box-lrg pure-g">
-              <div className="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
-                  <img width="300" alt="File Icons" className="pure-img-responsive" src="img/common/file-icons.png"/>
-              </div>
-              <div className="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
+            <div className="ribbon l-box-lrg pure-g">
+                <div className="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
+                    <img width="300" alt="File Icons" className="pure-img-responsive" src="img/common/file-icons.png"/>
+                </div>
+                <div className="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
 
-                  <h2 className="content-head content-head-ribbon">Laboris nisi ut aliquip.</h2>
+                    <h2 className="content-head content-head-ribbon">Laboris nisi ut aliquip.</h2>
 
-                  <p>
+                    <p>
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                       consequat. Duis aute irure dolor.
-                  </p>
-              </div>
-          </div>
+                    </p>
+                </div>
+            </div>
 
-          <div className="content">
-              <h2 className="content-head is-center">Dolore magna aliqua. Uis aute irure.</h2>
+            <div className="content">
+                <h2 className="content-head is-center">Dolore magna aliqua. Uis aute irure.</h2>
 
-              <div className="pure-g">
-                  <div className="l-box-lrg pure-u-1 pure-u-md-2-5">
-                      <form className="pure-form pure-form-stacked">
-                          <fieldset>
+                <div className="pure-g">
+                    <div className="l-box-lrg pure-u-1 pure-u-md-2-5">
+                        <form className="pure-form pure-form-stacked">
+                            <fieldset>
 
-                              <label htmlFor="name">Your Name</label>
-                              <input id="name" type="text" placeholder="Your Name"/>
+                                <label htmlFor="name">Your Name</label>
+                                <input id="name" type="text" placeholder="Your Name"/>
 
 
-                              <label htmlFor="email">Your Email</label>
-                              <input id="email" type="email" placeholder="Your Email"/>
+                                <label htmlFor="email">Your Email</label>
+                                <input id="email" type="email" placeholder="Your Email"/>
 
-                              <label htmlFor="password">Your Password</label>
-                              <input id="password" type="password" placeholder="Your Password"/>
+                                <label htmlFor="password">Your Password</label>
+                                <input id="password" type="password" placeholder="Your Password"/>
 
-                              <button type="submit" className="pure-button">Sign Up</button>
-                          </fieldset>
-                      </form>
-                  </div>
+                                <button type="submit" className="pure-button">Sign Up</button>
+                            </fieldset>
+                        </form>
+                    </div>
 
-                  <div className="l-box-lrg pure-u-1 pure-u-md-3-5">
-                      <h4>Contact Us</h4>
-                      <p>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                          consequat.
-                      </p>
+                    <div className="l-box-lrg pure-u-1 pure-u-md-3-5">
+                        <h4>Contact Us</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat.
+                        </p>
 
-                      <h4>More Information</h4>
-                      <p>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                          tempor incididunt ut labore et dolore magna aliqua.
-                      </p>
-                  </div>
-              </div>
+                        <h4>More Information</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="footer l-box is-center">
+                View the source of this layout to learn more. Made with love by the YUI Team.
+            </div>
 
-          </div>
-
-          <div className="footer l-box is-center">
-              View the source of this layout to learn more. Made with love by the YUI Team.
-          </div>
-
-      </div>
-</div>
+        </div>
+    </div>
 
    );
   }
