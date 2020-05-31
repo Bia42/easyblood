cdbd930784efe1e77503bbbda1b8c47faf9f5fec
Beatriz Oliveira <beatrizo237@gmail.com>
2020-05-31 19:21:05 -0300

    Final

--- src/componentes/patrocinadores.component.jsx
+++ src/componentes/patrocinadores.component.jsx
@@ -6,9 +6,8 @@ export const Card = props => (
 
     <div className="col-md-4 col-sm-6 patrocinadores-item">
         <a className="patrocinadores-link" data-toggle="modal" href="#patrocinadoresModal1">
-       <div className="patrocinadores-hover" />       
-
-         <img src={"data:image/jpeg;base64," + props.patrocinador.logo} alt="patrocinador_img" className = "img-fluid" /> 
+       <div className="patrocinadores-hover"/>       
+             <img src={"data:image/jpeg;base64," + props.patrocinador.logo} alt="patrocinador_img" className = "img-fluid" id="firstname" /> 
 
         </a>
         <div className="patrocinadores-caption">
