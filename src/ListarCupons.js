dc7ead3bfada9ceaabeed0c290874633ad548d56
Beatriz Oliveira <beatrizo237@gmail.com>
2020-05-30 21:11:47 -0300

    Isso é tudo pessoal

---
+++ src/ListarCupons.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+import './css/main.css';
+import './css/util.css';
+import './css/fonts/font-awesome-4.7.0/css/font-awesome.min.css';
+import './css/fonts/Linearicons-Free-v1.0.0/icon-font.min.css';
+import './css/vendor/animate/animate.css';
+import axios from 'axios';
+import Header from './componentes/Header';
+import * as utils from "./utils/utils";
+
+
+
+class ListarCupons extends Component {
+    constructor(props){
+        super(props)
+        this.state = {msg:'',
+            cuponsGerados: [],
+            cuponsResgatados:[]
+        };
+
+        axios.post('/rest/patrocinador/listCuponsAtivosPorPatrocinador',{
+            patrocinadorId:  localStorage.getItem("Dados"),
+            })
+            .then(response => {
+             console.log(response.data);
+             this.setState({cuponsGerados: response.data});
+
+            })
+            .catch(e=> {
+             // console.log("e.resp:");
+                 console.log(e.response.status);
+                console.log(e.response.data.cupom);
+               
+        });
+
+        
+        axios.post('/rest/patrocinador/listCuponsResgatadosPorPatrocinador',{
+            patrocinadorId:  localStorage.getItem("Dados"),
+            })
+            .then(response => {
+             console.log(response.data);
+             this.setState({cuponsResgatados: response.data});
+
+            })
+            .catch(e=> {
+             // console.log("e.resp:");
+                 console.log(e.response.status);
+                console.log(e.response.data.cupom);
+               
+        });
+            
+
+
+	}
+
+    
+    render(){
+        return (
+        
+         <div> 
+         <Header/>
+         <div className="limiter">
+             <div className="container-login100">
+                 <div className="wrap-login100 p-l-50 p-r-50 p-t-77 p-b-30">
+                     <form className="login100-form validate-form" >
+                         <span className="login100-form-title p-b-55">
+                             Listagem de Cupons
+                         </span>
+                      
+                        <h5>Cupons Ativos:</h5>
+                         <select>
+                         {
+                             this.state.cuponsGerados.map((cupom, i) => <option key={i}>{cupom.cupom}</option>)
+                         }
+                         </select>   
+
+                         <h5>Cupons Resgatados:</h5>
+                         <select>
+                         {
+                             this.state.cuponsResgatados.map((cupom, i) => <option key={i}>{cupom.cupom}</option>)
+                         }
+                         </select>   
+                     </form>
+                 </div>
+             </div>
+         </div>
+         </div>  
+        );
+       }
+}
+
+export default ListarCupons;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -13,6 +13,12 @@ export function novoRequestInfo(body) {
     };
 }
 
+export function usuarioLogado() {
+    var patrocinador =localStorage.getItem('Dados');
+
+    return patrocinador;
+}
+
 export function validaCPF(strCPF) {
     var Soma;
     var Resto;
@@ -44,20 +50,9 @@ export function validaTelefone(strTel) {
 }
 
 
-export function imageToBase64(img)
-{
-    var canvas, ctx, dataURL, base64;
-    canvas = document.createElement("canvas");
-    ctx = canvas.getContext("2d");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
-    dataURL = canvas.toDataURL("image/png");
-    base64 = dataURL.replace(/^data:image\/png;base64,/, "");
-    return base64;
-}
-
 export function logout() {
     localStorage.removeItem("Authorization");
+    localStorage.removeItem("Dados");
+
 }
 
